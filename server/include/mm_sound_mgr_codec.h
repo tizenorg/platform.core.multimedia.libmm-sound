4060fdc73f3f515050d8b80a74a1632d8c44d935
inhyeok <i_bc.kim@samsung.com>
2015-07-16 21:10:26 +0900

    apply focus for playing tone/wav
    [Version] Release 0.9.243
    [Profile] Common
    [Issue Type] Refactoring
    As ASM deprecated in 3.0, focus concept is applied for playing tone/wav internally.
    
    Change-Id: I88e28ace7819526c3ee38c82c7550636a224f597
    Signed-off-by: inhyeok <i_bc.kim@samsung.com>

--- server/include/mm_sound_mgr_codec.h
+++ server/include/mm_sound_mgr_codec.h
@@ -32,6 +32,9 @@ typedef struct {
 	int session_type;
 	int session_options;
 	int session_handle;
+	int focus_handle;
+	int focus_wcb_id;
+	int keytone;
 	void *stopcb;
 	int (*callback)(int, void *, void *, int);
 	void *msgcallback;		/* Client callback function */
@@ -60,6 +63,7 @@ int MMSoundMgrCodecFini(void);
 int MMSoundMgrCodecPlay(int *slotid, const mmsound_mgr_codec_param_t *param);
 int MMSoundMgrCodecPlayWithStreamInfo(int *slotid, const mmsound_mgr_codec_param_t *param);
 int MMSoundMgrCodecStop(const int slotid);
+int MMSoundMgrCodecClearFocus(const int slotid);
 int MMSoundMgrCodecCreate(int *slotid, const mmsound_mgr_codec_param_t *param);
 int MMSoundMgrCodecPlayWave(int slotid, const mmsound_mgr_codec_param_t *param);
 int MMSoundMgrCodecPlayDtmf(int *slotid, const mmsound_mgr_codec_param_t *param);
