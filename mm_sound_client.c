ba07648b50a3ed437256d7e068d4950740887959
inhyeok <i_bc.kim@samsung.com>
2015-07-21 13:12:44 +0900

    Adapt session interrupt callback to focus callback
    [Version] Release 0.9.242
    [Profile] Common
    [Issue Type] Refactoring
    [Dependency module] sound-manager
    
    Modify session interrupt callback to be invoked when focus callback is called.
    
    Change-Id: I7ee4fd874f58220dcb7aa2f46876b6a0c452b545
    Signed-off-by: inhyeok <i_bc.kim@samsung.com>

--- mm_sound_client.c
+++ mm_sound_client.c
@@ -573,6 +573,28 @@ int mm_sound_client_get_audio_path(mm_sound_device_in *device_in, mm_sound_devic
 
 
 #ifdef USE_FOCUS
+void mm_sound_client_set_session_interrupt_callback(mm_sound_focus_session_interrupt_cb callback)
+{
+	debug_fenter();
+
+	mm_sound_client_dbus_set_session_interrupt_callback(callback);
+
+	debug_fleave();
+	return;
+}
+
+int mm_sound_client_unset_session_interrupt_callback(void)
+{
+	int ret = MM_ERROR_NONE;
+	
+	debug_fenter();
+
+	ret = mm_sound_client_dbus_unset_session_interrupt_callback();
+
+	debug_fleave();
+	return ret;
+}
+
 int mm_sound_client_get_uniq_id(int *id)
 {
 	static int uniq_id = 0;
